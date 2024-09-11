89427c9fa10cf66329dcfe1c9032894070fe1f34
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-09-10 22:52:31 -0500

    correcion de manejo de rutas con expresss y script de deploy

--- server/src/app.js
+++ server/src/app.js
@@ -16,13 +16,12 @@ app.use(
 app.use(morgan("combined"));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
 app.use(launchesRouter);
 
 
-
-app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
